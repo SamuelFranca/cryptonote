2546390123a85f8d0ba76425942b9b0db84ddada
samuelfarnca <samuelfranca2@gmail.com>
2018-04-07 00:07:11 +0100

    new seed

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,6 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "54.194.63.89:18210",
+  "34.245.108.184:18210",
   "52.233.157.118:18210"  
 };
 
